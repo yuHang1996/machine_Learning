0d092c8a5fa83b887376e5281c1571705ad8134a
wangjiawei1996 <906789088@qq.com>
2026-06-25 16:02:45 +0800

    first commit

---
+++ vs-project/first.py
@@ -0,0 +1,32 @@
+import numpy;
+import matplotlib;
+import sklearn
+import pandas
+
+
+# print("Hello world1!")
+
+# age  = 29;
+# if (age > 18):
+#     print("You are an adult.")
+# else:
+#     print("You are a minor.")
+
+# birth = int(input("Enter your birth year: "))
+# if (birth < 2000):
+#     print("You are born in the 20th century.")
+# else:    print("You are born in the 21st century.")
+
+# str = input("Enter your name: ")
+# match str:
+#     case "Alice":
+#         print("Hello, Alice!")
+#     case "Bob":
+#         print("Hello, Bob!")
+#     case _:
+#         print("Hello, stranger!")
+
+def printFunc(a):
+    print(a)
+
+printFunc("Hello, World!")
